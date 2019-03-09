62dc2711c23e212f1bc7ba6716c9c42fc9fd1159
nuno-chicoria <nuno.chicoria@icloud.com>
2019-03-09 17:34:22 +0100

    the beginning

---
+++ rsa_ss.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Code for a Feed Forward Neural Network for the prediction of RSA and SS using
+frequency table of MSAs as features and RSA and SS as labels.
+
+@author: nuno_chicoria
+"""
+
