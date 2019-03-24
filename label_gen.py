2736afc4f57cc75a454e14f675cb0038e48df1e6
nuno-chicoria <nuno.chicoria@icloud.com>
2019-03-24 15:29:40 +0100

    code for the secundary structure label matrices

--- label_gen.py
+++ label_gen.py
@@ -12,24 +12,9 @@ frequency table of MSAs as features and RSA and SS as labels.
 import os
 import numpy as np
 
-#os.chdir("/Users/nuno_chicoria/Documents/master_thesis/rsa_matrices")
-
-#rsa = open("/Users/nuno_chicoria/Documents/master_thesis/rsa_ss/ACCpro.dssp.txt", "r")
-
-#for line in rsa.readlines():
-    #line = line.strip("\n")
-    #if line.startswith(">"):
-        #name = line[1:]
-    #else:
-        #rsa_matrix = np.zeros((len(line), 1))
-        #for i in range(len(line)):
-            #if line[i] == "e":
-                #rsa_matrix[i, 0] = 1
-        #np.save(name + ".npy", rsa_matrix)
-
-os.chdir("/Users/nuno_chicoria/Documents/master_thesis/ss_matrices")
-
-rsa = open("/Users/nuno_chicoria/Documents/master_thesis/rsa_ss/SSpro.dssp.txt", "r")
+#For loop for the matrices regarding relative solvent accessibility
+os.chdir("/Users/nuno_chicoria/Documents/master_thesis/rsa_matrices")
+rsa = open("/Users/nuno_chicoria/Documents/master_thesis/rsa_ss/ACCpro.dssp.txt", "r")
 
 for line in rsa.readlines():
     line = line.strip("\n")
@@ -41,3 +26,22 @@ for line in rsa.readlines():
             if line[i] == "e":
                 rsa_matrix[i, 0] = 1
         np.save(name + ".npy", rsa_matrix)
+
+#For loop for the matrices regarding secundary structure
+os.chdir("/Users/nuno_chicoria/Documents/master_thesis/ss_matrices")
+ss = open("/Users/nuno_chicoria/Documents/master_thesis/rsa_ss/SSpro.dssp.txt", "r")
+
+for line in ss.readlines():
+    line = line.strip("\n")
+    if line.startswith(">"):
+        name = line[1:]
+    else:
+        ss_matrix = np.zeros((len(line), 3))
+        for i in range(len(line)):
+            if line[i] == "C":
+                ss_matrix[i, 0] = 1
+            elif line[i] == "E":
+                ss_matrix[i, 1] = 1
+            else:
+                ss_matrix[i, 2] = 1
+        np.save(name + ".npy", ss_matrix)
